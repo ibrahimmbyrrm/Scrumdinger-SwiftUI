27c69532d16635745aac28c443980cce371d2b51
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2024-01-05 19:44:41 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by İbrahim Bayram on 5.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
